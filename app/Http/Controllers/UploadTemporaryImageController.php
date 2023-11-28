86a5d6dfe281b0d59ccfb77555a9a7fa50cb9973
Egzon Sylejmani <73958434+egzonsylejmani@users.noreply.github.com>
2023-11-28 21:31:55 +0100

    Update Upload Controller

--- app/Http/Controllers/UploadTemporaryImageController.php
+++ app/Http/Controllers/UploadTemporaryImageController.php
@@ -12,6 +12,19 @@ class UploadTemporaryImageController extends Controller
      */
     public function __invoke(Request $request)
     {
-    
+        if($request->hasFile('image')){
+            $image = $request->file('image');
+            $fileName = $image->getClientOriginalName();
+            $folder = uniqid('image-', true);
+            $image->storeAs('images/tmp/'. $folder , $fileName);
+
+            TemporaryImage::create([
+                'folder' => $folder,
+                'file' =>   $fileName
+            ]);
+
+            return $folder;
+        }
+        return 'Upload Error';
     }
 }
